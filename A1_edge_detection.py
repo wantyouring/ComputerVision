669f2cfd8383b0002931b5ff5ae5e41cf3424dcc
WonCheol Park <37647326+wantyouring@users.noreply.github.com>
2019-10-13 22:49:12 +0900

    complete except 2-2.

---
+++ A1_edge_detection.py
@@ -0,0 +1,20 @@
+from modules import *
+
+def main():
+    print('-----a1_edge_detection shapes.PNG start-----')
+    input_img_file_name = 'shapes.PNG'
+    gray = open_img(input_img_file_name)
+    a1_edge_detection_result(input_img_file_name,gray) # a1_edge_detection 결과 출력함수
+    print('-----a1_edge_detection shapes.PNG end-----')
+
+    print('-----a1_edge_detection lenna.PNG start-----')
+    input_img_file_name = 'lenna.PNG'
+    gray = open_img(input_img_file_name)
+    a1_edge_detection_result(input_img_file_name, gray)  # a1_edge_detection 결과 출력함수
+    print('-----a1_edge_detection lenna.PNG end-----')
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ A1_image_filtering.py
@@ -0,0 +1,21 @@
+from modules import *
+
+def main():
+    print('-----a1_image_filtering shapes.PNG start-----')
+    input_img_file_name = 'shapes.PNG'
+    gray = open_img(input_img_file_name)
+    a1_image_filtering_result(input_img_file_name,gray) # a1_image_filtering 결과 출력함수
+    print('-----a1_image_filtering shapes.PNG end-----')
+
+    print('-----a1_image_filtering lenna.PNG start-----')
+    input_img_file_name = 'lenna.PNG'
+    gray = open_img(input_img_file_name)
+    a1_image_filtering_result(input_img_file_name, gray)  # a1_image_filtering 결과 출력함수
+    print('-----a1_image_filtering lenna.PNG end-----')
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
