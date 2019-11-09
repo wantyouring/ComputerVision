8a91b6584098609f316d59487bd405e465248c4d
wantyouring <pwc99@naver.com>
2019-11-10 00:24:38 +0900

    finish draft of transformation functions

--- assignment2/A2_2d_transformations.py
+++ assignment2/A2_2d_transformations.py
@@ -6,22 +6,69 @@ def main():
     gray = open_img(input_img_file_name)
     M = [[1,0,0],[0,1,0],[0,0,1]]
     plane = get_transformed_image(gray,M) # 초기상태 plane
+
     cv2.imshow('test', plane)
-    cv2.waitKey()
+    cv2.waitKey(1)
 
     while True:
         pressed_key = input("put key : ")
         plane_draw = np.copy(plane) # 그림 계속 그리며 출력할 plane
+        # x,y축 이동
         if pressed_key == 'a':
-            #plane_draw = get_transformed_image(gray, M)
-            1
-        elif pressed_key == 'x':
-            M = np.dot([[0.95,0,0],[0,0.95,0],[0,0,1]],M)
+            M = np.dot(M_MOVE_MINUS_X,M)
             plane_draw = get_transformed_image(gray,M)
-        cv2.arrowedLine(plane_draw, xy_cor(-400, 0), xy_cor(400, 0), 0)
-        cv2.arrowedLine(plane_draw, xy_cor(0, -400), xy_cor(0, 400), 0)
+        elif pressed_key == 'd':
+            M = np.dot(M_MOVE_PLUS_X, M)
+            plane_draw = get_transformed_image(gray, M)
+        elif pressed_key == 'w':
+            M = np.dot(M_MOVE_PLUS_Y,M)
+            plane_draw = get_transformed_image(gray,M)
+        elif pressed_key == 's':
+            M = np.dot(M_MOVE_MINUS_Y,M)
+            plane_draw = get_transformed_image(gray,M)
+        # 회전이동
+        elif pressed_key == 'r':
+            M = np.dot(M_ROTATE_COUNTER_CLOCK,M)
+            plane_draw = get_transformed_image(gray,M)
+        elif pressed_key == 'R':
+            M = np.dot(M_ROTATE_CLOCK,M)
+            plane_draw = get_transformed_image(gray,M)
+        # axis flip
+        elif pressed_key == 'f':
+            M = np.dot(M_FLIP_Y_AXIS,M)
+            plane_draw = get_transformed_image(gray,M)
+        elif pressed_key == 'F':
+            M = np.dot(M_FLIP_X_AXIS,M)
+            plane_draw = get_transformed_image(gray, M)
+        # x,y방향 확대, 축소
+        elif pressed_key == 'x':
+            M = np.dot(M_SHRINK_X,M)
+            plane_draw = get_transformed_image(gray,M)
+        elif pressed_key == 'X':
+            M = np.dot(M_ENLARGE_X, M)
+            plane_draw = get_transformed_image(gray, M)
+        elif pressed_key == 'y':
+            M = np.dot(M_SHRINK_Y, M)
+            plane_draw = get_transformed_image(gray, M)
+        elif pressed_key == 'Y':
+            M = np.dot(M_ENLARGE_Y, M)
+            plane_draw = get_transformed_image(gray, M)
+        # 초기상태
+        elif pressed_key == 'H':
+            M = M_IDENTITY
+            plane_draw = np.copy(plane)
+        # 종료
+        elif pressed_key == 'Q':
+            cv2.destroyAllWindows()
+            return
+
+        cv2.arrowedLine(plane_draw, ij(400, 0), ij(-400, 0), 0) # 함수 자체에서 xy로 받아 순서 바꿔줬음
+        cv2.arrowedLine(plane_draw, ij(0, 400), ij(0, -400), 0)
+
+        # cv2.arrowedLine(plane_draw, xy_cor(-400, 0), xy_cor(400, 0), 0)
+        # cv2.arrowedLine(plane_draw, xy_cor(0, -400), xy_cor(0, 400), 0)
         cv2.imshow('test',plane_draw)
-        cv2.waitKey()
+        cv2.waitKey(1)
 
     print('-----A2_2d_transformations end-----')
 
--- assignment2/modules.py
+++ assignment2/modules.py
@@ -4,6 +4,50 @@ import time
 import os
 from math import *
 
+M_IDENTITY = [[1,0,0],
+              [0,1,0],
+              [0,0,1]]
+
+M_MOVE_MINUS_X = [[1,0,0],
+                  [0,1,0],
+                  [-5,0,1]]
+M_MOVE_PLUS_X = [[1,0,0],
+                 [0,1,0],
+                 [5,0,1]]
+M_MOVE_MINUS_Y = [[1,0,0],
+                  [0,1,0],
+                  [0,-5,1]]
+M_MOVE_PLUS_Y = [[1,0,0],
+                 [0,1,0],
+                 [0,5,1]]
+
+M_ROTATE_COUNTER_CLOCK = [[cos(5*pi/180), sin(5*pi/180), 0],
+                          [-sin(5*pi/180), cos(5*pi/180), 0],
+                          [0,0,1]]
+M_ROTATE_CLOCK = [[cos(-5*pi/180), sin(-5*pi/180), 0],
+                  [-sin(-5*pi/180), cos(-5*pi/180), 0],
+                  [0,0,1]]
+
+M_FLIP_X_AXIS = [[1,0,0],
+                 [0,-1,0],
+                 [0,0,1]]
+M_FLIP_Y_AXIS = [[-1,0,0],
+                 [0,1,0],
+                 [0,0,1]]
+
+M_SHRINK_X = [[0.95,0,0],
+              [0,1,0],
+              [0,0,1]]
+M_ENLARGE_X = [[1.05, 0, 0],
+               [0, 1, 0],
+               [0, 0, 1]]
+M_SHRINK_Y = [[1, 0, 0],
+              [0, 0.95, 0],
+              [0, 0, 1]]
+M_ENLARGE_Y = [[1, 0, 0],
+               [0, 1.05, 0],
+               [0, 0, 1]]
+
 # 이미지파일 열기.
 def open_img(input_img_file_name):
     if not os.path.exists('./result'):
@@ -15,9 +59,10 @@ def open_img(input_img_file_name):
 
 def ij(x,y):
     # 400,400 -> 0,0
-    return 400+y, 400-x
+    # return 400+y, 400-x
+    return 400 - y, 400 + x
 def xy(i,j):
-    return 400-j, i-400
+    return j-400, 400-i
 
 def xy_cor(x,y):
     return x+400,-y+400
@@ -48,7 +93,7 @@ def get_transformed_image(img,M):
     for i in range(0,h):
         for j in range(0,w):
             if img[i][j] != 1:
-                img_xy.append([int(w/2)-j,i-int(h/2),1])
+                img_xy.append([j-int(w/2),int(h/2)-i,1])
     print("h:{},w:{}".format(h,w))
     print(img_xy)
     print("img_xy shape : {} \n M shape : {}".format(np.shape(img_xy),np.shape(M)))
@@ -61,9 +106,4 @@ def get_transformed_image(img,M):
     result_plane = paint(plane,result_xy)
     print("result_plane type : {}".format(np.shape(result_plane)))
 
-    # smile_at = []
-    # for i in range(0, h):
-    #     for j in range(0, w):
-    #         if img[i][j] != 0:
-    #             smile_at.append([i, j])
     return result_plane
