0eae382c33e693e2c355e47927179902dac20183
WonCheol Park <37647326+wantyouring@users.noreply.github.com>
2019-12-20 18:32:33 +0900

    Fix bug, learn well

--- assignment3/A3_compute_descriptors.py
+++ assignment3/A3_compute_descriptors.py
@@ -35,18 +35,12 @@ N = 1000
 D = 1000
 
 sifts = [] # (1000,x,128)
-features = np.zeros((760512,128),dtype=np.float32)
-clusters = np.zeros((D,),dtype=int)
-_feature_i = 0
 
 N_np = np.array([N],dtype=np.int32)
 D_np = np.array([D],dtype=np.int32)
 d = np.zeros((N,D),dtype=np.float32)
 
-print(N_np)
-print(D_np)
-
-# sift들 불러오기, kmeans clustering용 붙여서 저장.
+# sift들 불러오기, kmeans clustering용 저장.
 for i in range(100000,101000):
     with open("./sift/sift{}".format(i), "rb") as f:
         sift = []
@@ -55,48 +49,17 @@ for i in range(100000,101000):
         np_sift = np.asarray(sift)
         np_sift = np.reshape(np_sift, (int(len(sift) / 128), 128))
         sifts.append(np_sift)
-        features[_feature_i:_feature_i + int(len(sift) / 128),0:128] = np_sift.copy()
-        _feature_i += int(len(sift) / 128)
-
-print(np.shape(sifts))
 
 centers = np.zeros((D,128),dtype=np.float32) # 중심 좌표들 저장. (D,128)
 
 # 1줄 평균
 for i in range(D):
-    centers[i,:] = np.sum(sifts[i][:,:]/128,axis=0).copy() # 2.67
-
-# # 1줄 + 4개 평균
-# for i in range(D):
-#     if i%4==0:
-#         tmp = np.zeros((128))
-#         for j in range(4):
-#             tmp += np.sum(sifts[i+j][0,:]/512,axis=0) # 수정 필요!
-#         for j in range(4):
-#             if i+j>=D:
-#                 break
-#             centers[i+j,:] = tmp.copy()
-
-# # 4개 평균
-# for i in range(D):
-#     if i%4==0:
-#         tmp = np.zeros((128))
-#         for j in range(4):
-#             tmp += sifts[i+j][0,:]/4
-#
-#         for j in range(4):
-#             if i+j>=D:
-#                 break
-#             centers[i+j,:] = tmp.copy()
-
-# 중심 임의로 D개.(각 이미지들 처음 feature로 하기)
-# centers[:,:] = features[0:D,:].copy()
-
-clusters = [] # (1000,x)
+    centers[i,:] = np.sum(sifts[i][:,:]/(len(sifts[i])),axis=0).copy() # 2.67
 
 # k means 학습
-Learn = 20
+Learn = 3
 for i in range(Learn): # 학습 횟수
+    clusters = []  # (1000,x)
     _d = np.zeros((N, D), dtype=np.float32)
     for j in range(N):
         cluster = np.zeros(len(sifts[j]),dtype=int)
@@ -112,7 +75,7 @@ for i in range(Learn): # 학습 횟수
         clusters.append(cluster)
 
     # descriptor 파일 만들기.
-    with open("./A3_2014312993_{}.des".format(i), "wb") as f:
+    with open("./A3_2014312993_{}_{}.des".format(D,i), "wb") as f:
         f.write(N_np.tobytes())
         f.write(D_np.tobytes())
         f.write(_d.tobytes())
@@ -121,16 +84,11 @@ for i in range(Learn): # 학습 횟수
     clusters_cnt = np.zeros((D))
     clusters_avg = np.zeros((D, 128))
 
-    print("len(clusters[j]):{}".format(len(clusters[j])))
-
     for j in range(N):
         for k in range(len(clusters[j])):
             clusters_avg[clusters[j][k]] += sifts[j][k]
             clusters_cnt[clusters[j][k]] += 1
     centers = (clusters_avg / np.reshape((clusters_cnt+np.finfo(float).eps),(-1,1))).copy()
-
-print(d)
-print(np.sum(d))
 
 # descriptor 파일 만들기.
 with open("./A3_2014312993.des", "wb") as f:
